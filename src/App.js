bdf1bd1dc613dda5f4b52e035faaca952c3372f6
Senthil-Bala <senthilkumarbala100@gmail.com>
2023-12-23 17:38:10 +0530

    appp  crreated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Countries from './Countries';
 
 function App() {
   return (
     <div className="App">
-      
+      <Countries/>
     </div>
   );
 }
